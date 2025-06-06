2edd2ee1b0d8d0bca17eeb6bb7f0661d6db5f2fd
Shridhar Savant <93203347+shri1918@users.noreply.github.com>
2025-06-06 10:02:59 +0530

    Update utils.ts

--- lib/utils.ts
+++ lib/utils.ts
@@ -9,6 +9,6 @@ export function cn(...inputs: ClassValue[]) {
 export const aviationApi = axios.create({
   baseURL: "https://api.aviationstack.com/v1/",
   params: {
-    access_key: process.env.NEXT_PUBLIC_AVIATIONSTACK_API_KEY,
+    access_key: d7a7e77d48285a908b863bd98aabf378,
   },
 })
